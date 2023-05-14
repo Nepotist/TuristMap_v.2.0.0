09d81b81aa93a66d7eb329b585d8c469160e35aa
Nepotist <128513718+Nepotist@users.noreply.github.com>
2023-05-14 16:56:51 +0700

    Add files via upload

---
+++ data/Skiing_11.js
@@ -0,0 +1,11 @@
+var json_Skiing_11 = {
+"type": "FeatureCollection",
+"name": "Skiing_11",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "Мрас-Су", "Фото": "<img src=\"images/гкмрас.jpg\" align=”middle” width='250' height='150'>" }, "geometry": { "type": "Point", "coordinates": [ 87.846126319440472, 53.704401438705652 ] } },
+{ "type": "Feature", "properties": { "name": "Танай", "Фото": "<img src=\"images/танай.jpg\" align=”middle” width='250' height='150'>" }, "geometry": { "type": "Point", "coordinates": [ 85.066969759105234, 54.716905824008357 ] } },
+{ "type": "Feature", "properties": { "name": "Горная Саланга", "Фото": "<img src=\"images/саланга.jpg\" align=”middle” width='250' height='150'>" }, "geometry": { "type": "Point", "coordinates": [ 88.536393781465932, 55.126190496308531 ] } },
+{ "type": "Feature", "properties": { "name": "Шерегеш", "Фото": "<img src=\"images/шерегеш.jpg\" align=”middle” width='250' height='150'>" }, "geometry": { "type": "Point", "coordinates": [ 87.952402966464106, 52.953564041234522 ] } }
+]
+}
